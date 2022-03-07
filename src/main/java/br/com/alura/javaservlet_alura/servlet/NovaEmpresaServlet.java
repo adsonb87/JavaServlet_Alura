d8973745ab2e82d00ec2e3a8f4866cb76bc744e7
adsonb87 <adsonb87@gmail.com>
2022-03-07 16:59:23 -0300

    Atualizando servlets

--- src/main/java/br/com/alura/javaservlet_alura/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/javaservlet_alura/servlet/NovaEmpresaServlet.java
@@ -44,10 +44,12 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		//Chamar o JSP
-		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
-		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request, response);
+		response.sendRedirect("listaEmpresas");
+		
+//		//Chamar o JSP
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 	}
 
 }
