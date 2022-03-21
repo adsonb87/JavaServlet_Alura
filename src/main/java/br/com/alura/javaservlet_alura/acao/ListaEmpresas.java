89d9ab6941f1f80b2960cbe5ed30c8ca7a84e4d3
adsonb87 <adsonb87@gmail.com>
2022-03-21 16:10:44 -0300

    Ajustes login]

--- src/main/java/br/com/alura/javaservlet_alura/acao/ListaEmpresas.java
+++ src/main/java/br/com/alura/javaservlet_alura/acao/ListaEmpresas.java
@@ -1,12 +1,10 @@
 package br.com.alura.javaservlet_alura.acao;
 
-import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.javaservlet_alura.modelo.Banco;
 import br.com.alura.javaservlet_alura.modelo.Empresa;
@@ -14,6 +12,9 @@ import br.com.alura.javaservlet_alura.modelo.Empresa;
 public class ListaEmpresas implements Acao{
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) {
+				
+		
+		
 		System.out.println("Listando empresas");
 		
 		Banco banco = new Banco();
--- src/main/java/br/com/alura/javaservlet_alura/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/javaservlet_alura/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.javaservlet_alura.acao.Acao;
 
@@ -18,6 +19,18 @@ public class UnicaEntradaServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
     	String paramAcao = request.getParameter("acao");
+    	
+    	HttpSession sessao = request.getSession();
+		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
+		
+		//Todas as ações que não forem referentes a login não serão permitidas.
+    	boolean acaoProtegida = !paramAcao.equals("Login") || paramAcao.equals("LoginForm");
+    	
+		if(acaoProtegida && usuarioNaoEstaLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+    	 	
     	
     	String nomeDaClasse = "br.com.alura.javaservlet_alura.acao." + 
     							paramAcao;
